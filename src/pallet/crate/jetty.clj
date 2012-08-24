b80ad0578035b0c7f034a55baf7ce346031ee10e
Hugo Duncan <hugo@hugoduncan.org>
2012-08-24 13:56:13 -0400

    Update to use settings

--- src/pallet/crate/jetty.clj
+++ src/pallet/crate/jetty.clj
@@ -1,6 +1,8 @@
 (ns pallet.crate.jetty
   "Installation of jetty."
   (:require
+   [clojure.string :as string]
+   [clojure.tools.logging :as logging]
    [pallet.action :as action]
    [pallet.action.directory :as directory]
    [pallet.action.exec-script :as exec-script]
@@ -10,73 +12,192 @@
    [pallet.action.service :as service]
    [pallet.action.user :as user]
    [pallet.crate.etc-default :as etc-default]
-   [pallet.crate.java :as java]
    [pallet.parameter :as parameter]
-   [pallet.stevedore :as stevedore]
-   [clojure.string :as string])
-  (:use pallet.thread-expr))
+   [pallet.stevedore :as stevedore])
+  (:use
+   pallet.thread-expr
+   [pallet.action.directory :only [directory]]
+   [pallet.action.exec-script :only [exec-script]]
+   [pallet.action.file :only [file]]
+   [pallet.action.remote-directory :only [remote-directory]]
+   [pallet.action.remote-file :only [remote-file]]
+   [pallet.common.context :only [throw-map]]
+   [pallet.parameter :only [assoc-target-settings get-target-settings]]
+   [pallet.utils :only [apply-map]]
+   [pallet.version-dispatch
+    :only [defmulti-version-crate defmulti-version defmulti-os-crate
+           multi-version-session-method multi-version-method
+           multi-os-session-method]]
+   [pallet.versions :only [version-string]]))
 
-(def install-path "/usr/share/jetty7")
-(def log-path "/var/log/jetty7")
-(def jetty-user "jetty")
-(def jetty-group "jetty")
+(def ^{:doc "Flag for recognising changes to configuration"}
+  jetty-config-changed-flag "jetty-config")
 
 ;; 7.1.4.v20100610
 ;; 7.0.2.v20100331
-(def default-version "7.0.2.v20100331")
+;; 8.1.5.v20120716
+(def ^:dynamic *default-version* "8.1.5.v20120716")
 
-(defn download-path [version]
-  (format
-   "http://download.eclipse.org/jetty/%s/dist/jetty-distribution-%s.tar.gz"
-   version version))
+(def ^:dynamic *eclipse-jetty-url*
+  "http://download.eclipse.org/jetty/%1$s/dist/jetty-distribution-%1$s.tar.gz")
 
-(defn jetty
+(defn- download-path [version]
+  (format *eclipse-jetty-url* version))
+
+(def ^:dynamic *jetty-defaults*
+  {:user "jetty"
+   :group "jetty"
+   :log-path "/var/log/jetty"
+   :install-path "/usr/share/jetty"
+   :webapps "/usr/share/jetty/webapps/"
+   :service "jetty"})
+
+;;; Based on supplied settings, decide which install strategy we are using
+;;; for jetty.
+
+(defmulti-version-crate jetty-version-settings [version session settings])
+
+(multi-version-session-method
+    jetty-version-settings {:os :linux}
+    [os os-version version session settings]
+  (cond
+    (:strategy settings) settings
+    (:download settings) (assoc settings :strategy :download)
+    :else (let [dl (download-path (:version settings))]
+            (assoc settings
+              :strategy :download
+              :download {:url dl :md5-url (str dl ".md5")}))))
+
+;;; ## Settings
+(defn- settings-map
+  "Dispatch to either openjdk or oracle settings"
+  [session settings]
+  (jetty-version-settings
+   session (:version settings) (merge *jetty-defaults* settings)))
+
+(defn jetty-settings
+  "Capture settings for jetty
+
+:user
+:group
+:home
+:version"
+  [session {:keys [version user group install-path instance-id]
+            :or {version *default-version*}
+            :as settings}]
+  (let [settings (settings-map session (merge {:version version} settings))]
+    (assoc-target-settings session :jetty instance-id settings)))
+
+
+(defmulti install-method (fn [session settings] (:strategy settings)))
+
+(defmethod install-method :download
+  [session {:keys [download install-path log-path user group webapps]}]
+  (->
+   session
+   (user/group group :system true)
+   (user/user user :system true :shell "/bin/false" :group group)
+   (apply-map-> remote-directory install-path :user user :group group download)
+   (directory webapps :owner user :group group)
+   (directory (str install-path "/logs") :owner user :group group)
+   (directory log-path :owner user :group group)
+   (file (str install-path "/contexts/test.xml") :action :delete :force true)
+   (file (str install-path "/contexts/demo.xml") :action :delete :force true)
+   (file (str install-path "/contexts/javadoc.xml") :action :delete :force true)
+   (file (str install-path "/webapps/test.war") :action :delete :force true)
+   (directory (str install-path "/contexts/test.d")
+              :action :delete :recursive true :force true)))
+
+(defn install-jetty
+  "Install jetty."
+  [session & {:keys [instance-id]}]
+  (let [settings (get-target-settings session :jetty instance-id ::no-settings)]
+    (logging/debugf "install-jetty settings %s" settings)
+    (if (= settings ::no-settings)
+      (throw-map
+       "Attempt to install jetty without specifying settings"
+       {:message "Attempt to install jetty without specifying settings"
+        :type :invalid-operation})
+      (install-method session settings))))
+
+
+(defn install-jetty-service
   "Install jetty via download"
-  [session & {:as options}]
-  (let [path (download-path (:version options default-version))]
+  [session  & {:keys [instance-id]}]
+  (let [{:keys [user install-path log-path service no-enable]}
+        (get-target-settings session :jetty instance-id ::no-settings)]
     (->
      session
-     (parameter/assoc-for
-      [:jetty :base] install-path
-      [:jetty :owner] jetty-user
-      [:jetty :group] jetty-group)
-     (java/java :openjdk)
-     (user/group jetty-group :system true)
-     (user/user jetty-user :system true :shell "/bin/false")
-     (remote-directory/remote-directory
-      install-path
-      :url path :unpack :tar :tar-options "xz"
-      :owner jetty-user :group jetty-group)
-     (directory/directory
-      (str install-path "/webapps") :owner jetty-user :group jetty-group)
-     (directory/directory
-      (str install-path "/logs") :owner jetty-user :group jetty-group)
-     (directory/directory
-      log-path :owner jetty-user :group jetty-group)
      (etc-default/write
       "jetty7"
-      "JETTY_USER" jetty-user
+      "JETTY_USER" user
       "JETTY_LOGS" log-path
-      "JETTY_HOME" install-path
-      "JAVA_HOME" "/usr/lib/jvm/java-6-openjdk")
+      "JETTY_HOME" install-path)
      (service/init-script
-      "jetty"
-      :remote-file (str install-path "/bin/jetty.sh"))
-     (when-not-> (:no-enable options)
-       (service/service "jetty" :action :enable))
-     ;; Remove the default webapps
-     (file/file
-      (str install-path "/contexts/test.xml") :action :delete :force true)
-     (file/file
-      (str install-path "/contexts/demo.xml") :action :delete :force true)
-     (file/file
-      (str install-path "/contexts/javadoc.xml") :action :delete :force true)
-     (file/file
-      (str install-path "/webapps/test.war") :action :delete :force true)
-     (directory/directory
-      (str install-path "/contexts/test.d")
-      :action :delete :recursive true :force true)
-     (service/service "jetty" :action :enable))))
+      service :remote-file (str install-path "/bin/jetty.sh"))
+     (when-not-> no-enable
+       (service/service "jetty" :action :enable)))))
+
+(defn init-service
+  "Control the jetty service.
+
+   Specify `:if-config-changed true` to make actions conditional on a change in
+   configuration.
+
+   Other options are as for `pallet.action.service/service`. The service
+   name is looked up in the request parameters."
+  [session & {:keys [action if-config-changed if-flag instance-id] :as options}]
+  (let [{:keys [service]} (get-target-settings session :jetty instance-id)
+        options (if if-config-changed
+                  (assoc options :if-flag jetty-config-changed-flag)
+                  options)]
+    (-> session (apply-map-> service/service service options))))
+
+;; (defn jetty
+;;   "Install jetty via download"
+;;   [session & {:as options}]
+;;   (let [path (download-path (:version options default-version))]
+;;     (->
+;;      session
+;;      (parameter/assoc-for
+;;       [:jetty :base] install-path
+;;       [:jetty :owner] jetty-user
+;;       [:jetty :group] jetty-group)
+;;      (java/java :openjdk)
+;;      (remote-directory/remote-directory
+;;       install-path
+;;       :url path :unpack :tar :tar-options "xz"
+;;       :owner jetty-user :group jetty-group)
+;;      (directory/directory
+;;       (str install-path "/webapps") :owner jetty-user :group jetty-group)
+;;      (directory/directory
+;;       (str install-path "/logs") :owner jetty-user :group jetty-group)
+;;      (directory/directory
+;;       log-path :owner jetty-user :group jetty-group)
+;;      (etc-default/write
+;;       "jetty7"
+;;       "JETTY_USER" jetty-user
+;;       "JETTY_LOGS" log-path
+;;       "JETTY_HOME" install-path
+;;       "JAVA_HOME" "/usr/lib/jvm/java-6-openjdk")
+;;      (service/init-script
+;;       "jetty"
+;;       :remote-file (str install-path "/bin/jetty.sh"))
+;;      (when-not-> (:no-enable options)
+;;        (service/service "jetty" :action :enable))
+;;      ;; Remove the default webapps
+;;      (file/file
+;;       (str install-path "/contexts/test.xml") :action :delete :force true)
+;;      (file/file
+;;       (str install-path "/contexts/demo.xml") :action :delete :force true)
+;;      (file/file
+;;       (str install-path "/contexts/javadoc.xml") :action :delete :force true)
+;;      (file/file
+;;       (str install-path "/webapps/test.war") :action :delete :force true)
+;;      (directory/directory
+;;       (str install-path "/contexts/test.d")
+;;       :action :delete :recursive true :force true)
+;;      (service/service "jetty" :action :enable))))
 
 
 (def remote-file* (action/action-fn remote-file/remote-file-action))
@@ -87,40 +208,51 @@
    the server configuration file."
   {:arglists '([session option-string])}
   [session options]
-  (stevedore/chain-commands
-   (directory*
-    session
-    (str install-path "/etc") :owner jetty-user :group jetty-group)
-   (remote-file*
-    session
-    (str install-path "/etc/jetty.conf")
-    :content (string/join \newline (map first options)))))
+  (let [{:keys [user group install-path]}
+        (get-target-settings
+         session :jetty (:instance-id (ffirst options)) ::no-settings)]
+    (stevedore/chain-commands
+     (directory* session (str install-path "/etc") :owner user :group group)
+     (remote-file*
+      session
+      (str install-path "/etc/jetty.conf")
+      :content (string/join \newline (map first options))
+      :flag-on-changed jetty-config-changed-flag))))
 
 (defn server
   "Configure the jetty server (jetty.xml)."
-  [session content]
-  (remote-file/remote-file
-   session
-   (str install-path "/etc/jetty.xml")
-   :content content)) ;; (configure "etc/jetty.xml") ; read by start.jar
+  [session content & {:keys [instance-id]}]
+  (let [{:keys [user group install-path]}
+        (get-target-settings session :jetty instance-id ::no-settings)]
+    (remote-file/remote-file
+     session
+     (str install-path "/etc/jetty.xml")
+     :content content :user user :group group
+     :flag-on-changed jetty-config-changed-flag)))
 
 (defn ssl
   "Configure an ssl connector (jetty-ssl.xml)."
-  [session content]
-  (->
-   session
-   (remote-file/remote-file
-    (str install-path "/etc/jetty-ssl.xml")
-    :content content)
-   (configure "etc/jetty-ssl.xml")))
+  [session content & {:keys [instance-id]}]
+  (let [{:keys [user group install-path]}
+        (get-target-settings session :jetty instance-id ::no-settings)]
+    (->
+     session
+     (remote-file/remote-file
+      (str install-path "/etc/jetty-ssl.xml")
+      :content content :user user :group group
+      :flag-on-changed jetty-config-changed-flag)
+     (configure "etc/jetty-ssl.xml"))))
 
 (defn context
   "Configure an application context"
-  [session name content]
-  (remote-file/remote-file
-   session
-   (str install-path "/contexts/" name ".xml")
-   :content content))
+  [session name content & {:keys [instance-id]}]
+  (let [{:keys [user group install-path]}
+        (get-target-settings session :jetty instance-id ::no-settings)]
+    (remote-file/remote-file
+     session
+     (str install-path "/contexts/" name ".xml")
+     :content content
+     :flag-on-changed jetty-config-changed-flag)))
 
 (defn deploy
   "Copies a .war file to the jetty server under webapps/${app-name}.war.  An
@@ -130,12 +262,13 @@
 
    Other Options:
      :clear-existing true -- removes an existing exploded ${app-name} directory"
-  [session app-name & {:as opts}]
-  (let [exploded-app-dir (str install-path "/webapps/" (or app-name "ROOT"))
+  [session app-name & {:keys [instance-id] :as opts}]
+  (let [{:keys [user group install-path webapps]}
+        (get-target-settings session :jetty instance-id ::no-settings)
+        exploded-app-dir (str webapps "/" (or app-name "ROOT"))
         deployed-warfile (str exploded-app-dir ".war")
-        options (merge
-                 {:owner jetty-user :group jetty-group :mode 600}
-                 (select-keys opts remote-file/content-options))]
+        options (merge {:owner user :group group :mode 600}
+                       (select-keys opts remote-file/content-options))]
     (->
      session
      (when-not->
@@ -146,12 +279,9 @@
         directory/directory
         exploded-app-dir
         (apply concat
-               (merge {:owner jetty-user :group jetty-group :recursive true}
+               (merge {:owner user :group group :recursive true}
                       (select-keys options [:owner :group :recursive])))))
      (apply->
-      remote-file/remote-file
-      deployed-warfile
-      (apply concat options))
+      remote-file/remote-file deployed-warfile (apply concat options))
      (when-> (:clear-existing opts)
-       (exec-script/exec-script
-        (rm ~exploded-app-dir ~{:r true :f true}))))))
+       (exec-script/exec-script (rm ~exploded-app-dir ~{:r true :f true}))))))
--- test/pallet/crate/jetty_test.clj
+++ test/pallet/crate/jetty_test.clj
@@ -1,17 +1,118 @@
 (ns pallet.crate.jetty-test
   (:use
+   [pallet.parameter :only [get-target-settings]]
+   [pallet.parameter-test :only [settings-test]]
    clojure.test
    pallet.test-utils)
   (:require
+   [pallet.action :as action]
+   [pallet.action.directory :as directory]
+   [pallet.action.exec-script :as exec-script]
+   [pallet.action.file :as file]
+   [pallet.action.package :as package]
+   [pallet.action.remote-file :as remote-file]
+   [pallet.blobstore :as blobstore]
    [pallet.build-actions :as build-actions]
-   [pallet.crate.jetty :as jetty]))
+   [pallet.compute :as compute]
+   [pallet.core :as core]
+   [pallet.crate.automated-admin-user :as automated-admin-user]
+   [pallet.crate.java :as java]
+   [pallet.crate.jetty :as jetty]
+   [pallet.crate.network-service :as network-service]
+   [pallet.enlive :as enlive]
+   [pallet.live-test :as live-test]
+   [pallet.parameter :as parameter]
+   [pallet.parameter-test :as parameter-test]
+   [pallet.phase :as phase]
+   [pallet.script.lib :as lib]
+   [pallet.session :as session]
+   [pallet.stevedore :as stevedore]
+   [pallet.thread-expr :as thread-expr]))
 
 (deftest invoke-test
   (is (build-actions/build-actions
        {}
-       (jetty/jetty)
+       (jetty/jetty-settings {})
+       (jetty/install-jetty)
        (jetty/configure "")
        (jetty/server "")
        (jetty/ssl "")
        (jetty/context "" "")
        (jetty/deploy "" :content "c"))))
+
+(def settings-map {})
+
+(def jetty-unsupported
+  [])
+
+(def ^{:doc "An html file for jetty to serve to verify we have it running."}
+  index-html
+  "<html>
+<head>
+  <title>Pallet-live-test</title>
+  <%@ page language=\"java\" %>
+</head>
+<body>
+<h3><%= java.net.InetAddress.getLocalHost().getHostAddress() %></h3>
+<h3><%= java.lang.System.getProperty(\"java.vendor\") %></h3>
+<h3><%= java.lang.System.getProperty(\"java.runtime.name\") %></h3>
+<h3><%= java.lang.System.getProperty(\"java.vm.name\") %></h3>
+LIVE TEST
+</body></html>")
+
+(def app-name "pallet-live-test")
+
+(def
+  ^{:doc "An application configuration context"}
+  application-config
+  (format "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<Configure class=\"org.eclipse.jetty.webapp.WebAppContext\">
+  <Set name=\"contextPath\">/%1$s</Set>
+  <Set name=\"resourceBase\"><SystemProperty name=\"jetty.home\" default=\".\"/>/webapps/%1$s</Set>
+</Configure>"
+          app-name))
+
+(deftest live-test
+  (live-test/test-for
+   [image (live-test/exclude-images (live-test/images) jetty-unsupported)]
+   (live-test/test-nodes
+    [compute node-map node-types]
+    {:jetty
+     {:image image
+      :count 1
+      :phases {:bootstrap (phase/phase-fn
+                           (automated-admin-user/automated-admin-user))
+               :settings (phase/phase-fn
+                          (java/java-settings {})
+                          (jetty/jetty-settings settings-map))
+               :configure (fn [session]
+                            (let [settings (get-target-settings
+                                            session :jetty nil)
+                                  appdir (str (:webapps settings) app-name "/")]
+                              (->
+                               session
+                               (java/install-java)
+                               (jetty/install-jetty)
+                               (jetty/install-jetty-service)
+                               ;; (jetty/server)
+                               (jetty/context app-name application-config)
+                               (directory/directory appdir)
+                               (remote-file/remote-file
+                                (str appdir "index.jsp")
+                                :content index-html :literal true
+                                :flag-on-changed
+                                jetty/jetty-config-changed-flag)
+                               (jetty/init-service
+                                :if-config-changed true
+                                :action :restart))))
+               :verify (phase/phase-fn
+                        (network-service/wait-for-http-status
+                         (format "http://localhost:8080/%s/" app-name)
+                         200 :url-name "jetty server")
+                        (exec-script/exec-checked-script
+                         "check jetty is running with  jdk"
+                         (pipe
+                          (wget "-O-"
+                                (str "http://localhost:8080/" ~app-name "/"))
+                          (grep -i (quoted "")))))}}}
+    (core/lift (:jetty node-types) :phase :verify :compute compute))))
