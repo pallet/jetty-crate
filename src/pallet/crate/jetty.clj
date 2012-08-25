0fbe42599f2e9f504c005853b5a7abfb37d1df2f
Hugo Duncan <hugo@hugoduncan.org>
2012-08-25 10:23:36 -0400

    Cleanup namespaces

--- src/pallet/crate/jetty.clj
+++ src/pallet/crate/jetty.clj
@@ -4,10 +4,7 @@
    [clojure.string :as string]
    [clojure.tools.logging :as logging]
    [pallet.action :as action]
-   [pallet.action.directory :as directory]
-   [pallet.action.exec-script :as exec-script]
    [pallet.action.file :as file]
-   [pallet.action.remote-directory :as remote-directory]
    [pallet.action.remote-file :as remote-file]
    [pallet.action.service :as service]
    [pallet.action.user :as user]
@@ -16,13 +13,16 @@
    [pallet.stevedore :as stevedore])
   (:use
    pallet.thread-expr
+   [pallet.action :only [action-fn def-collected-action]]
    [pallet.action.directory :only [directory]]
    [pallet.action.exec-script :only [exec-script]]
    [pallet.action.file :only [file]]
    [pallet.action.remote-directory :only [remote-directory]]
-   [pallet.action.remote-file :only [remote-file]]
+   [pallet.action.remote-file :only [remote-file content-options]]
    [pallet.common.context :only [throw-map]]
+   [pallet.core :only [server-spec]]
    [pallet.parameter :only [assoc-target-settings get-target-settings]]
+   [pallet.phase :only [phase-fn]]
    [pallet.utils :only [apply-map]]
    [pallet.version-dispatch
     :only [defmulti-version-crate defmulti-version defmulti-os-crate
@@ -153,57 +153,10 @@
                   options)]
     (-> session (apply-map-> service/service service options))))
 
-;; (defn jetty
-;;   "Install jetty via download"
-;;   [session & {:as options}]
-;;   (let [path (download-path (:version options default-version))]
-;;     (->
-;;      session
-;;      (parameter/assoc-for
-;;       [:jetty :base] install-path
-;;       [:jetty :owner] jetty-user
-;;       [:jetty :group] jetty-group)
-;;      (java/java :openjdk)
-;;      (remote-directory/remote-directory
-;;       install-path
-;;       :url path :unpack :tar :tar-options "xz"
-;;       :owner jetty-user :group jetty-group)
-;;      (directory/directory
-;;       (str install-path "/webapps") :owner jetty-user :group jetty-group)
-;;      (directory/directory
-;;       (str install-path "/logs") :owner jetty-user :group jetty-group)
-;;      (directory/directory
-;;       log-path :owner jetty-user :group jetty-group)
-;;      (etc-default/write
-;;       "jetty7"
-;;       "JETTY_USER" jetty-user
-;;       "JETTY_LOGS" log-path
-;;       "JETTY_HOME" install-path
-;;       "JAVA_HOME" "/usr/lib/jvm/java-6-openjdk")
-;;      (service/init-script
-;;       "jetty"
-;;       :remote-file (str install-path "/bin/jetty.sh"))
-;;      (when-not-> (:no-enable options)
-;;        (service/service "jetty" :action :enable))
-;;      ;; Remove the default webapps
-;;      (file/file
-;;       (str install-path "/contexts/test.xml") :action :delete :force true)
-;;      (file/file
-;;       (str install-path "/contexts/demo.xml") :action :delete :force true)
-;;      (file/file
-;;       (str install-path "/contexts/javadoc.xml") :action :delete :force true)
-;;      (file/file
-;;       (str install-path "/webapps/test.war") :action :delete :force true)
-;;      (directory/directory
-;;       (str install-path "/contexts/test.d")
-;;       :action :delete :recursive true :force true)
-;;      (service/service "jetty" :action :enable))))
+(def remote-file* (action-fn remote-file/remote-file-action))
+(def directory* (action-fn directory))
 
-
-(def remote-file* (action/action-fn remote-file/remote-file-action))
-(def directory* (action/action-fn directory/directory))
-
-(action/def-collected-action configure
+(def-collected-action configure
   "Configure jetty options for jetty.conf.  Each argument will be added to
    the server configuration file."
   {:arglists '([session option-string])}
@@ -224,7 +177,7 @@
   [session content & {:keys [instance-id]}]
   (let [{:keys [user group install-path]}
         (get-target-settings session :jetty instance-id ::no-settings)]
-    (remote-file/remote-file
+    (remote-file
      session
      (str install-path "/etc/jetty.xml")
      :content content :user user :group group
@@ -237,7 +190,7 @@
         (get-target-settings session :jetty instance-id ::no-settings)]
     (->
      session
-     (remote-file/remote-file
+     (remote-file
       (str install-path "/etc/jetty-ssl.xml")
       :content content :user user :group group
       :flag-on-changed jetty-config-changed-flag)
@@ -248,7 +201,7 @@
   [session name content & {:keys [instance-id]}]
   (let [{:keys [user group install-path]}
         (get-target-settings session :jetty instance-id ::no-settings)]
-    (remote-file/remote-file
+    (remote-file
      session
      (str install-path "/contexts/" name ".xml")
      :content content
@@ -268,7 +221,7 @@
         exploded-app-dir (str webapps "/" (or app-name "ROOT"))
         deployed-warfile (str exploded-app-dir ".war")
         options (merge {:owner user :group group :mode 600}
-                       (select-keys opts remote-file/content-options))]
+                       (select-keys opts content-options))]
     (->
      session
      (when-not->
@@ -276,12 +229,11 @@
        ;; if we're not removing an existing, try at least to make sure
        ;; that jetty has the permissions to explode the war
       (apply->
-        directory/directory
+        directory
         exploded-app-dir
         (apply concat
                (merge {:owner user :group group :recursive true}
                       (select-keys options [:owner :group :recursive])))))
-     (apply->
-      remote-file/remote-file deployed-warfile (apply concat options))
+     (apply-> remote-file deployed-warfile (apply concat options))
      (when-> (:clear-existing opts)
-       (exec-script/exec-script (rm ~exploded-app-dir ~{:r true :f true}))))))
+       (exec-script (rm ~exploded-app-dir ~{:r true :f true}))))))
